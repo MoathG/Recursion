7e53b78fea07e914e8c9aa165ec7537c4214c53d
MoathG <gharibmoath@gmail.com>
2019-07-09 17:45:54 +0300

    exercise 4 done recursion

--- recursion.js
+++ recursion.js
@@ -9,8 +9,14 @@ Ex: sum(5); => 5 + 4 + 3 + 2 + 1 + 0 => 15
 
 */
 function sum(n) {
-  // your code here 
+  
+  if (n === 0) {
+    return 0;
+  }
+  return n + sum(n-1);
 }
+
+sum(1);
 
 /*
 2
@@ -18,13 +24,19 @@ Create a function called factorial
 that takes a single parameter n, 
 and return the product of all integers up to n starting from 1
 
-Ex: factorial(2); => 2 * 1 => 6 
+Ex: factorial(2); => 2 * 1 => 2 
 Ex: factorial(4); => 4 * 3 * 2 * 1 => 24 
 
 */
 function factorial(n) {
-  // your code here 
+  
+    if ( n === 1 ) {
+      return 1;
+    }
+    return n * factorial(n -1);
 }
+
+factorial(2);
 
 
 /*
@@ -36,6 +48,18 @@ and return the string number time with space
 Ex: repeatStr("to",2); => "to to"
 Ex: repeatStr("to",4); => "to to to to"
 */
+
+
+function repeatStr (str, num) {
+
+  if (num === 0) {
+    return "";
+
+  }
+  return str + " " + repeatStr(str, num-1);
+}
+
+repeatStr('to', 4);
 
 
 /*
@@ -54,108 +78,59 @@ fib(n): 1 1 2 3 5 8 13 21
 n:      0 1 2 3 4 5  6  7
 */
 
+function fibon (x) {
+  
+  if ( x <= 1 ) {
+    return x = 1;
+  }
+  return fibon(x - 1) + fibon(x - 2) 
+}
+
+fibon(7);
+
+
+
+
+
 
 /*
-
-
-
-
+5
+Create a function called sum2
+that take two parameter
+and will return the sumation from the first number to the second number
+Ex: sum2(4, 5); => 4 + 5 => 9
+Ex: sum2(3, 6); => 3 + 4 + 5 + 6 => 18
 */
 
 
 /*
-
-
-
-
+6
+Create a function called repeatStr2
+that takes 2 string parameters,
+and return the first string number time
+(of the lenght of the secend string) with space betweeen it
+Important: the stop condition should be [somthing=== 0]
+Ex: repeatStr2("ro","cc"); => "ro ro"
+Ex: repeatStr2("ro","fff"); => "ro ro ro"
 */
 
 
 /*
-
-
-
-
+7
+Create a function called multiOf
+that takes 3 parameters,
+and return the first number mutiple by
+the second one ^ the third one
+Ex: multiOf(4,3,10); => 4000
+Ex: multiOf(6,2,4); => 96
 */
 
 
 /*
-
-
-
-
+8
+Create a function called muti2
+that take two parameter
+and will return the multiplication from the first number to the second number
+Ex: muti2(4, 5); => 4 * 5 => 20
+Ex: muti2(3, 6); => 3 * 4 * 5 * 6 => 360
 */
-
-
-/*
-
-
-
-
-*/
-
-
-/*
-
-
-
-
-*/
-
-
-/*
-
-
-
-
-*/
-
-
-/*
-
-
-
-
-*/
-
-
-/*
-
-
-
-
-*/
-
-
-/*
-
-
-
-
-*/
-
-
-/*
-
-
-
-
-*/
-
-
-/*
-
-
-
-
-*/
-
-
-/*
-
-
-
-
-*/
-
-
